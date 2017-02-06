8b4fd91b7ccc21afd644ae85d738cd255f92a7bf
Nguyen Hoang Son <nhson@apcs.vn>
2017-02-06 20:28:36 +0700

    test

--- cmd/dotastatsWeb/main.go
+++ cmd/dotastatsWeb/main.go
@@ -102,7 +102,7 @@ func main() {
 	r.Get("/crawl", common.Then(a.Wrap(a.GetCustomCrawlHandler())))
 	handler := cors.Default().Handler(r)
 	c := cron.New()
-	c.AddFunc("@every 1s", func() {
+	c.AddFunc("@every 5m", func() {
 		err = a.RunCrawlerAndSave()
 		if err != nil {
 			fmt.Println("error running crawler %s", err)
