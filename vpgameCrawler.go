1e2f657b5e2f8a61ea6190d669f16bc692396a34
Nguyen Hoang Son <nhson@apcs.vn>
2017-03-15 09:29:40 +0700

    add series id

--- vpgameCrawler.go
+++ vpgameCrawler.go
@@ -129,6 +129,7 @@ func RunCrawlerVpgame(vpParams VPGameAPIParams) ([]Match, error) {
 		matchFinal.TournamentLogo = LogoURL + match.Tournament.Logo
 		matchFinal.LogoA = LogoURL + match.Team.Left.Logo
 		matchFinal.LogoB = LogoURL + match.Team.Right.Logo
+		matchFinal.SeriesID = match.SeriesID
 		seriesParam := VPGameAPIParams{TID: match.SeriesID}
 		respSeries, err := VPGameGet(SeriesAPI, seriesParam)
 		if err != nil {
